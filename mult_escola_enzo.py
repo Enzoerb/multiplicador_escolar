4a7716ec1a3eaefe32422d67b727ec097930ceb2
Enzo Erbert <enzo.erbert@semantix.com.br>
2020-05-15 11:58:58 -0300

    now you can multiply floats

--- mult_escola_enzo.py
+++ mult_escola_enzo.py
@@ -1,34 +1,83 @@
-def mid_operation(multiplicador, multiplicando):
-    signal = 1
+def signal(number):
+    number_signal = 1
 
-    multiplicador = str(multiplicador)
-    multiplicando = str(multiplicando)
-    if multiplicador[0] == '-':
-        signal *= -1
-        multiplicador = multiplicador.replace('-', '')
-    if multiplicando[0] == '-':
-        signal *= -1
-        multiplicando = multiplicando.replace('-', '')
+    if number < 0:
+        number_signal *= -1
 
-    higher = max((int(multiplicador), int(multiplicando)))
+    return number_signal
+
+
+def ten_potence(number):
+    number = float(number)
+    number = str(number)
+
+    period_location = number.find('.')
+
+    if int(float(number)) == float(number):
+        potence = (len(number)-2) - period_location
+    else:
+        potence = (len(number)-1) - period_location
+
+    number_ten_potence = 10**potence
+
+    return number_ten_potence
+
+
+def format_to_multiplication(number):
+
+    if int(number) == float(number):
+        number = int(number)
+    else:
+        number = int(str(number).replace('.', ''))
+
+    return abs(number)
+
+
+def mid_numbers(multiplicador, multiplicando):
+
+    higher = max((multiplicador, multiplicando))
     higher = str(higher)
 
-    lower = min((int(multiplicador), int(multiplicando)))
+    lower = min((multiplicador, multiplicando))
     lower = str(lower)
 
-    mid_numbers = [str(signal)]
+    mid_numbers = list()
     for place, index in enumerate(range(len(lower)-1, -1, -1)):
         parsial_result = 0
         for mult_place, mult_index in enumerate(range(len(higher)-1, -1, -1)):
             parsial_result += int(lower[index])*int(higher[mult_index])*(10**mult_place)
-        parsial_result = str(parsial_result) + ' '*place
+        parsial_result *= 10**place
         mid_numbers.append(parsial_result)
+
     return mid_numbers
 
 
-def final_sum(mid_numbers):
-    numbers_to_sum = [int(number.replace(' ', '0')) for number in mid_numbers[1:]]
-    result = sum(numbers_to_sum)*int(mid_numbers[0])
+def mid_operation(multiplicador, multiplicando):
+    multiplicador = float(multiplicador)
+    multiplicando = float(multiplicando)
+
+    mid_information = dict()
+    mid_information['signal'] = 1
+    mid_information['ten_potence'] = 1
+    mid_information['mid_numbers'] = list()
+
+    mid_information['signal'] = signal(multiplicador)*signal(multiplicando)
+    multiplicador = abs(multiplicador)
+    multiplicando = abs(multiplicando)
+
+    mid_information['ten_potence'] = ten_potence(multiplicador) * ten_potence(multiplicando)
+    multiplicador = format_to_multiplication(multiplicador)
+    multiplicando = format_to_multiplication(multiplicando)
+
+    mid_information['mid_numbers'] = mid_numbers(multiplicador, multiplicando)
+
+    return mid_information
+
+
+def final_sum(mid_information):
+    mid_sum = sum(mid_information['mid_numbers'])
+    mid_signal = mid_sum*mid_information['signal']
+    result = mid_signal / mid_information['ten_potence']
     return result
 
 
@@ -39,5 +88,7 @@ def school_multiplication(multiplicador, multiplicando):
 
 
 if __name__ == '__main__':
-    result = school_multiplication(-98765, -12345)
+    result = school_multiplication(98765, 12345)
+    print(result)
+    result = school_multiplication(1388739.81, -22740)
     print(result)
--- test_mult_escola_enzo.py
+++ test_mult_escola_enzo.py
@@ -1,62 +1,119 @@
 import unittest
 import mult_escola_enzo
-from random import randint, choice
+from random import randint
 
 
 class TestMultEscola(unittest.TestCase):
 
+    def test_signal(self):
+        self.assertEqual(mult_escola_enzo.signal(98765), 1)
+        self.assertEqual(mult_escola_enzo.signal(-98765), -1)
+        self.assertEqual(mult_escola_enzo.signal(123.45), 1)
+        self.assertEqual(mult_escola_enzo.signal(-123.45), -1)
+
+    def test_ten_potence(self):
+        self.assertEqual(mult_escola_enzo.ten_potence(1), 1)
+        self.assertEqual(mult_escola_enzo.ten_potence(10.0), 1)
+        self.assertEqual(mult_escola_enzo.ten_potence(100), 1)
+        self.assertEqual(mult_escola_enzo.ten_potence(0.1), 10)
+        self.assertEqual(mult_escola_enzo.ten_potence(0.01), 100)
+        self.assertEqual(mult_escola_enzo.ten_potence(0.001), 1000)
+
+    def test_format_to_multiplication(self):
+        function = mult_escola_enzo.format_to_multiplication
+        self.assertEqual(function(11.9), 119)
+        self.assertEqual(function(1234567.0), 1234567)
+        self.assertEqual(function(1234567.01), 123456701)
+        self.assertEqual(function(-1234567.0), 1234567)
+        self.assertEqual(function(-123.0120), 123012)
+
+    def test_mid_numbers(self):
+        self.assertEqual(mult_escola_enzo.mid_numbers(98765, 12345), [493825, 3950600, 29629500,
+                                                                      197530000, 987650000])
+        self.assertEqual(mult_escola_enzo.mid_numbers(98765, 0), [0])
+        self.assertEqual(mult_escola_enzo.mid_numbers(0, 12345), [0])
+        self.assertEqual(mult_escola_enzo.mid_numbers(0, 0), [0])
+
     def test_mid_operation(self):
-        first_result = ['1', '493825', '395060'+(' '*1),
-                        '296295'+(' '*2), '197530'+(' '*3), '98765'+(' '*4)]
-        second_result = ['-1'] + first_result[1:]
+        first_result = {'signal': 1,
+                        'ten_potence': 1,
+                        'mid_numbers': [493825, 3950600, 29629500,
+                                        197530000, 987650000]}
+        second_result = {key: first_result[key] for key in first_result}
+        second_result['signal'] = -1
+
+        third_result = {key: first_result[key] for key in first_result}
+        third_result['ten_potence'] = 100
+
+        fourth_result = {key: second_result[key] for key in second_result}
+        fourth_result['ten_potence'] = 1000
 
         self.assertEqual(mult_escola_enzo.mid_operation(98765, 12345), first_result)
         self.assertEqual(mult_escola_enzo.mid_operation(-98765, -12345), first_result)
         self.assertEqual(mult_escola_enzo.mid_operation(-98765, 12345), second_result)
         self.assertEqual(mult_escola_enzo.mid_operation(98765, -12345), second_result)
+        self.assertEqual(mult_escola_enzo.mid_operation(9876.5, 1234.5), third_result)
+        self.assertEqual(mult_escola_enzo.mid_operation(987.65, 12345), third_result)
+        self.assertEqual(mult_escola_enzo.mid_operation(98765, 123.45), third_result)
+        self.assertEqual(mult_escola_enzo.mid_operation(-987.65, 1234.5), fourth_result)
+        self.assertEqual(mult_escola_enzo.mid_operation(-98.765, 12345), fourth_result)
+        self.assertEqual(mult_escola_enzo.mid_operation(98765, -12.345), fourth_result)
 
         self.assertEqual(mult_escola_enzo.mid_operation('98765', '12345'), first_result)
         self.assertEqual(mult_escola_enzo.mid_operation('-98765', '-12345'), first_result)
         self.assertEqual(mult_escola_enzo.mid_operation('-98765', '12345'), second_result)
         self.assertEqual(mult_escola_enzo.mid_operation('98765', '-12345'), second_result)
+        self.assertEqual(mult_escola_enzo.mid_operation('9876.5', '1234.5'), third_result)
+        self.assertEqual(mult_escola_enzo.mid_operation('987.65', '12345'), third_result)
+        self.assertEqual(mult_escola_enzo.mid_operation('98765', '123.45'), third_result)
+        self.assertEqual(mult_escola_enzo.mid_operation('-987.65', '1234.5'), fourth_result)
+        self.assertEqual(mult_escola_enzo.mid_operation('-98.765', '12345'), fourth_result)
+        self.assertEqual(mult_escola_enzo.mid_operation('98765', '-12.345'), fourth_result)
 
-        self.assertEqual(mult_escola_enzo.mid_operation(0, 0), ['1', '0'])
+        self.assertEqual(mult_escola_enzo.mid_operation(0, 0),
+                         {'signal': 1, 'ten_potence': 1, 'mid_numbers': [0]})
 
     def test_final_sum(self):
-        first_input = ['1', '493825', '395060'+(' '*1),
-                       '296295'+(' '*2), '197530'+(' '*3), '98765'+(' '*4)]
-        second_input = ['-1'] + first_input[1:]
+        first_input = {'signal': 1,
+                       'ten_potence': 1,
+                       'mid_numbers': [493825, 3950600, 29629500,
+                                       197530000, 987650000]}
+        second_input = {key: first_input[key] for key in first_input}
+        second_input['signal'] = -1
+
+        third_input = {key: second_input[key] for key in second_input}
+        third_input['ten_potence'] = 100
+
+        fourth_input = {'signal': 1, 'ten_potence': 1, 'mid_numbers': [0]}
+        fifth_input = {'signal': -1, 'ten_potence': 10000, 'mid_numbers': [0]}
+
         self.assertEqual(mult_escola_enzo.final_sum(first_input), 1219253925)
         self.assertEqual(mult_escola_enzo.final_sum(second_input), -1219253925)
-        self.assertEqual(mult_escola_enzo.final_sum(['1', '0']), 0)
+        self.assertEqual(mult_escola_enzo.final_sum(third_input), -12192539.25)
+        self.assertEqual(mult_escola_enzo.final_sum(fourth_input), 0)
+        self.assertEqual(mult_escola_enzo.final_sum(fifth_input), 0)
 
     def test_school_multiplication(self):
         self.assertEqual(mult_escola_enzo.school_multiplication(98765, 12345), 1219253925)
+        self.assertEqual(mult_escola_enzo.school_multiplication(987.65, 1234.5), 1219253.925)
         self.assertEqual(mult_escola_enzo.school_multiplication(-98765, 12345), -1219253925)
-        self.assertEqual(mult_escola_enzo.school_multiplication(98765, -12345), -1219253925)
+        self.assertEqual(mult_escola_enzo.school_multiplication(987.65, -1.2345), -1219.253925)
         self.assertEqual(mult_escola_enzo.school_multiplication(-98765, -12345), 1219253925)
 
         self.assertEqual(mult_escola_enzo.school_multiplication('98765', '12345'), 1219253925)
+        self.assertEqual(mult_escola_enzo.school_multiplication('987.65', '1234.5'), 1219253.925)
         self.assertEqual(mult_escola_enzo.school_multiplication('-98765', '12345'), -1219253925)
+        self.assertEqual(mult_escola_enzo.school_multiplication('987.65', '-1.2345'), -1219.253925)
         self.assertEqual(mult_escola_enzo.school_multiplication('98765', '-12345'), -1219253925)
         self.assertEqual(mult_escola_enzo.school_multiplication('-98765', '-12345'), 1219253925)
 
         self.assertEqual(mult_escola_enzo.school_multiplication(0, 0), 0)
 
         for test in range(10):
-            number_1 = 0
-            number_2 = 0
-            for size in range(randint(1, 10)):
-                number_1 += randint(0, 9)*10**size
-                signal = choice([1, -1])
-                number_1 *= signal
+            number_1 = randint(-10**9, 10**9)
+            number_2 = randint(-10**9, 10**9)
 
-            for size in range(randint(1, 10)):
-                number_2 += randint(0, 9)*10**size
-                signal = choice([1, -1])
-                number_2 *= signal
-
-            multiplication = number_1 * number_2
+            multiplication = float(number_1 * number_2)
             self.assertEqual(mult_escola_enzo.school_multiplication(
                 number_1, number_2), multiplication)
             self.assertEqual(mult_escola_enzo.school_multiplication(
