1ddd81ab93bd565fda229d82dbebac47f57a2d1b
Enzo Erbert <enzo.erbert@semantix.com.br>
2020-05-22 11:50:26 -0300

    adding oop

--- mult_escola_enzo.py
+++ mult_escola_enzo.py
@@ -1,94 +1,137 @@
-def signal(number):
-    number_signal = 1
+class SchoolMult:
 
-    if number < 0:
-        number_signal *= -1
+    def __init__(self, multiplicador: str = '0', multiplicando: str = '0'):
+        self.multiplicador = multiplicador
+        self.multiplicando = multiplicando
+        self.mid_numbers = list()
+        self.result_signal = 1
+        self.potence = 0
+        self.mid_sum = '0'
+        self.result = '0'
 
-    return number_signal
+    @property
+    def higher_number(self):
+        if self.multiplicando > self.multiplicador:
+            return self.multiplicando
+        else:
+            return self.multiplicador
 
+    @property
+    def lower_number(self):
+        if self.multiplicando > self.multiplicador:
+            return self.multiplicador
+        else:
+            return self.multiplicando
 
-def ten_potence(number):
-    number = float(number)
-    number = str(number)
+    @staticmethod
+    def potence_format(number: str):
+        number = number.replace('-', '')
+        number = number.replace('+', '')
+        point_location = number.find('.')
 
-    period_location = number.find('.')
+        if point_location == -1:
+            potence = 0
+        else:
+            number = number.rstrip('0')
+            number = number.replace('.', '')
+            potence = len(number) - point_location
 
-    if int(float(number)) == float(number):
-        potence = (len(number)-2) - period_location
-    else:
-        potence = (len(number)-1) - period_location
+        return number, potence
 
-    number_ten_potence = 10**potence
+    def signal(self):
+        if self.multiplicador != '0' and self.multiplicando != '0':
+            self.result_signal = float(self.multiplicador)
+            self.result_signal *= float(self.multiplicando)
+            self.result_signal /= abs(self.result_signal)
+            self.result_signal = int(self.result_signal)
+        else:
+            self.result_signal = 1
 
-    return number_ten_potence
+    def ten_potence(self):
+        self.potence = 0
+        self.potence += self.potence_format(self.multiplicando)[1]
+        self.potence += self.potence_format(self.multiplicador)[1]
 
+    def mid_operation(self):
 
-def format_to_multiplication(number):
+        lower, _ = self.potence_format(self.lower_number)
+        lower = str(lower)
+        higher, _ = self.potence_format(self.higher_number)
+        higher = str(higher)
+        for index in range(len(lower)-1, -1, -1):
+            parsial_result = 0
+            for mult_place, mult_index in enumerate(range(len(higher)-1, -1, -1)):
+                parsial_mult = int(lower[index]) * int(higher[mult_index])
+                parsial_mult *= 10**mult_place
+                parsial_result += parsial_mult
+            parsial_result = str(parsial_result)
+            self.mid_numbers.append(parsial_result)
 
-    if int(number) == float(number):
-        number = int(number)
-    else:
-        number = int(str(number).replace('.', ''))
+        return self.mid_numbers
 
-    return abs(number)
+    def final_sum(self):
+        self.mid_sum = [int(number)*(10**index) for index, number in enumerate(self.mid_numbers)]
+        self.mid_sum = sum(self.mid_sum)
+        self.mid_sum = str(self.mid_sum)
+        self.result = self.include_decimal(self.mid_sum, 0)
+        self.result = '+'+self.result if self.result_signal > 0 else '-'+self.result
+        try:
+            self.result = '+0'+self.result[1:] if self.result[1] == '.' else self.result
+        except IndexError:
+            pass
 
+    def include_decimal(self, number, spaces):
+        if self.potence <= spaces:
+            if self.potence > 0:
+                number += ' '*(spaces+1)
+            else:
+                number += ' '*(spaces)
+        elif self.potence >= len(number) + spaces:
+            number = '0.' + '0'*(self.potence-(len(number)+spaces)) + number
+            number += ' '*spaces
+        else:
+            number = number + ' '*(spaces)
+            number = number[:-self.potence] + '.' + number[-self.potence:]
+        return number
 
-def mid_numbers(multiplicador, multiplicando):
+    def include_signal(self, number):
+        if number.find('-') != -1 or number.find('+') != -1:
+            number = number
+        elif self.result_signal < 0:
+            number = '-'+number
+        else:
+            number = '+'+number
+        return number
 
-    higher = max((multiplicador, multiplicando))
-    higher = str(higher)
+    def format_number_str(self, number, spaces):
+        number = self.include_decimal(number, spaces)
+        number = self.include_signal(number)
+        number = ' '*(len(self)-len(number))+number
+        return number
 
-    lower = min((multiplicador, multiplicando))
-    lower = str(lower)
+    def __len__(self):
+        return len(str(self.result))
 
-    mid_numbers = list()
-    for place, index in enumerate(range(len(lower)-1, -1, -1)):
-        parsial_result = 0
-        for mult_place, mult_index in enumerate(range(len(higher)-1, -1, -1)):
-            parsial_result += int(lower[index])*int(higher[mult_index])*(10**mult_place)
-        parsial_result *= 10**place
-        mid_numbers.append(parsial_result)
+    def __str__(self):
+        elements = ['  '+' '*(len(self)-len(self.higher_number))+self.higher_number,
+                    'x '+' '*(len(self)-len(self.lower_number))+self.lower_number,
+                    '---'+'-'*len(self)]
+        elements += ['  '+self.format_number_str(number, index)
+                     for index, number in enumerate(self.mid_numbers)]
 
-    return mid_numbers
+        elements += ['---'+'-'*len(self), '  '+self.result]
 
+        return '\n'.join(elements)
 
-def mid_operation(multiplicador, multiplicando):
-    multiplicador = float(multiplicador)
-    multiplicando = float(multiplicando)
-
-    mid_information = dict()
-    mid_information['signal'] = 1
-    mid_information['ten_potence'] = 1
-    mid_information['mid_numbers'] = list()
-
-    mid_information['signal'] = signal(multiplicador)*signal(multiplicando)
-    multiplicador = abs(multiplicador)
-    multiplicando = abs(multiplicando)
-
-    mid_information['ten_potence'] = ten_potence(multiplicador) * ten_potence(multiplicando)
-    multiplicador = format_to_multiplication(multiplicador)
-    multiplicando = format_to_multiplication(multiplicando)
-
-    mid_information['mid_numbers'] = mid_numbers(multiplicador, multiplicando)
-
-    return mid_information
-
-
-def final_sum(mid_information):
-    mid_sum = sum(mid_information['mid_numbers'])
-    mid_signal = mid_sum*mid_information['signal']
-    result = mid_signal / mid_information['ten_potence']
-    return result
-
-
-def school_multiplication(multiplicador, multiplicando):
-    mid_numbers = mid_operation(multiplicador, multiplicando)
-    result = final_sum(mid_numbers)
-    return result
+    def __repr__(self):
+        return f"SchoolMult('{self.multiplicador}', '{self.multiplicando}')"
 
 
 if __name__ == '__main__':
-    result = school_multiplication(98765, 12345)
-    print(result)
-    result = school_multiplication(1388739.81, -22740)
-    print(result)
+    Multiplication = SchoolMult('111', '111')
+    Multiplication.signal()
+    Multiplication.ten_potence()
+    Multiplication.mid_operation()
+    Multiplication.final_sum()
+    print(str(Multiplication))
+    print(len(Multiplication))
