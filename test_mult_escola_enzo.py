e784e2cdaf208f66578097971a914a06ff32f6b7
Enzo Erbert <enzo.erbert@semantix.com.br>
2020-05-22 11:47:36 -0300

    adding tests to oop

--- test_mult_escola_enzo.py
+++ test_mult_escola_enzo.py
@@ -5,119 +5,216 @@ from random import randint
 
 class TestMultEscola(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.testes = [mult_escola_enzo.SchoolMult('0', '0'),
+                      mult_escola_enzo.SchoolMult('4', '4'),
+                      mult_escola_enzo.SchoolMult('10', '10'),
+                      mult_escola_enzo.SchoolMult('111', '111'),
+                      mult_escola_enzo.SchoolMult('1'*9, '1'*9),
+                      mult_escola_enzo.SchoolMult('-'+'1'*9, '1'*9),
+                      mult_escola_enzo.SchoolMult('1'*3+'.'+'1'*6, '1'*9),
+                      mult_escola_enzo.SchoolMult('12345', '98765'),
+                      mult_escola_enzo.SchoolMult('-12345', '98765'),
+                      mult_escola_enzo.SchoolMult('-12345', '-98765'),
+                      mult_escola_enzo.SchoolMult('12.345', '98765'),
+                      mult_escola_enzo.SchoolMult('-12345', '98.765'),
+                      mult_escola_enzo.SchoolMult('123.45', '9876.5'),
+                      mult_escola_enzo.SchoolMult('-1.2345', '98.765')]
+        for teste in cls.testes:
+            teste.signal()
+            teste.ten_potence()
+            teste.mid_operation()
+            teste.final_sum()
+
     def test_signal(self):
-        self.assertEqual(mult_escola_enzo.signal(98765), 1)
-        self.assertEqual(mult_escola_enzo.signal(-98765), -1)
-        self.assertEqual(mult_escola_enzo.signal(123.45), 1)
-        self.assertEqual(mult_escola_enzo.signal(-123.45), -1)
+        self.assertEqual(self.testes[0].result_signal, 1)
+        self.assertEqual(self.testes[1].result_signal, 1)
+        self.assertEqual(self.testes[2].result_signal, 1)
+        self.assertEqual(self.testes[3].result_signal, 1)
+        self.assertEqual(self.testes[4].result_signal, 1)
+        self.assertEqual(self.testes[5].result_signal, -1)
+        self.assertEqual(self.testes[6].result_signal, 1)
+        self.assertEqual(self.testes[7].result_signal, 1)
+        self.assertEqual(self.testes[8].result_signal, -1)
+        self.assertEqual(self.testes[9].result_signal, 1)
+        self.assertEqual(self.testes[10].result_signal, 1)
+        self.assertEqual(self.testes[11].result_signal, -1)
+        self.assertEqual(self.testes[12].result_signal, 1)
+        self.assertEqual(self.testes[13].result_signal, -1)
 
     def test_ten_potence(self):
-        self.assertEqual(mult_escola_enzo.ten_potence(1), 1)
-        self.assertEqual(mult_escola_enzo.ten_potence(10.0), 1)
-        self.assertEqual(mult_escola_enzo.ten_potence(100), 1)
-        self.assertEqual(mult_escola_enzo.ten_potence(0.1), 10)
-        self.assertEqual(mult_escola_enzo.ten_potence(0.01), 100)
-        self.assertEqual(mult_escola_enzo.ten_potence(0.001), 1000)
+        self.assertEqual(self.testes[0].potence, 0)
+        self.assertEqual(self.testes[1].potence, 0)
+        self.assertEqual(self.testes[2].potence, 0)
+        self.assertEqual(self.testes[3].potence, 0)
+        self.assertEqual(self.testes[4].potence, 0)
+        self.assertEqual(self.testes[5].potence, 0)
+        self.assertEqual(self.testes[6].potence, 6)
+        self.assertEqual(self.testes[7].potence, 0)
+        self.assertEqual(self.testes[8].potence, 0)
+        self.assertEqual(self.testes[9].potence, 0)
+        self.assertEqual(self.testes[10].potence, 3)
+        self.assertEqual(self.testes[11].potence, 3)
+        self.assertEqual(self.testes[12].potence, 3)
+        self.assertEqual(self.testes[13].potence, 7)
 
-    def test_format_to_multiplication(self):
-        function = mult_escola_enzo.format_to_multiplication
-        self.assertEqual(function(11.9), 119)
-        self.assertEqual(function(1234567.0), 1234567)
-        self.assertEqual(function(1234567.01), 123456701)
-        self.assertEqual(function(-1234567.0), 1234567)
-        self.assertEqual(function(-123.0120), 123012)
-
-    def test_mid_numbers(self):
-        self.assertEqual(mult_escola_enzo.mid_numbers(98765, 12345), [493825, 3950600, 29629500,
-                                                                      197530000, 987650000])
-        self.assertEqual(mult_escola_enzo.mid_numbers(98765, 0), [0])
-        self.assertEqual(mult_escola_enzo.mid_numbers(0, 12345), [0])
-        self.assertEqual(mult_escola_enzo.mid_numbers(0, 0), [0])
+    def test_potence_format(self):
+        self.assertEqual(self.testes[0].potence_format('12345'), ('12345', 0))
+        self.assertEqual(self.testes[0].potence_format('-12345'), ('12345', 0))
+        self.assertEqual(self.testes[0].potence_format('+12345'), ('12345', 0))
+        self.assertEqual(self.testes[0].potence_format('1234.5'), ('12345', 1))
+        self.assertEqual(self.testes[0].potence_format('1.2345'), ('12345', 4))
 
     def test_mid_operation(self):
-        first_result = {'signal': 1,
-                        'ten_potence': 1,
-                        'mid_numbers': [493825, 3950600, 29629500,
-                                        197530000, 987650000]}
-        second_result = {key: first_result[key] for key in first_result}
-        second_result['signal'] = -1
-
-        third_result = {key: first_result[key] for key in first_result}
-        third_result['ten_potence'] = 100
-
-        fourth_result = {key: second_result[key] for key in second_result}
-        fourth_result['ten_potence'] = 1000
-
-        self.assertEqual(mult_escola_enzo.mid_operation(98765, 12345), first_result)
-        self.assertEqual(mult_escola_enzo.mid_operation(-98765, -12345), first_result)
-        self.assertEqual(mult_escola_enzo.mid_operation(-98765, 12345), second_result)
-        self.assertEqual(mult_escola_enzo.mid_operation(98765, -12345), second_result)
-        self.assertEqual(mult_escola_enzo.mid_operation(9876.5, 1234.5), third_result)
-        self.assertEqual(mult_escola_enzo.mid_operation(987.65, 12345), third_result)
-        self.assertEqual(mult_escola_enzo.mid_operation(98765, 123.45), third_result)
-        self.assertEqual(mult_escola_enzo.mid_operation(-987.65, 1234.5), fourth_result)
-        self.assertEqual(mult_escola_enzo.mid_operation(-98.765, 12345), fourth_result)
-        self.assertEqual(mult_escola_enzo.mid_operation(98765, -12.345), fourth_result)
-
-        self.assertEqual(mult_escola_enzo.mid_operation('98765', '12345'), first_result)
-        self.assertEqual(mult_escola_enzo.mid_operation('-98765', '-12345'), first_result)
-        self.assertEqual(mult_escola_enzo.mid_operation('-98765', '12345'), second_result)
-        self.assertEqual(mult_escola_enzo.mid_operation('98765', '-12345'), second_result)
-        self.assertEqual(mult_escola_enzo.mid_operation('9876.5', '1234.5'), third_result)
-        self.assertEqual(mult_escola_enzo.mid_operation('987.65', '12345'), third_result)
-        self.assertEqual(mult_escola_enzo.mid_operation('98765', '123.45'), third_result)
-        self.assertEqual(mult_escola_enzo.mid_operation('-987.65', '1234.5'), fourth_result)
-        self.assertEqual(mult_escola_enzo.mid_operation('-98.765', '12345'), fourth_result)
-        self.assertEqual(mult_escola_enzo.mid_operation('98765', '-12.345'), fourth_result)
-
-        self.assertEqual(mult_escola_enzo.mid_operation(0, 0),
-                         {'signal': 1, 'ten_potence': 1, 'mid_numbers': [0]})
+        self.assertEqual(self.testes[0].mid_numbers, ['0'])
+        self.assertEqual(self.testes[1].mid_numbers, ['16'])
+        self.assertEqual(self.testes[2].mid_numbers, ['0', '10'])
+        self.assertEqual(self.testes[3].mid_numbers, ['1'*3]*3)
+        result_4_till_6 = ['1'*9]*9
+        self.assertEqual(self.testes[4].mid_numbers, result_4_till_6)
+        self.assertEqual(self.testes[5].mid_numbers, result_4_till_6)
+        self.assertEqual(self.testes[6].mid_numbers, result_4_till_6)
+        result_7_till_13 = ['493825', '395060', '296295', '197530', '98765']
+        self.assertEqual(self.testes[7].mid_numbers, result_7_till_13)
+        self.assertEqual(self.testes[8].mid_numbers, result_7_till_13)
+        self.assertEqual(self.testes[9].mid_numbers, result_7_till_13)
+        self.assertEqual(self.testes[10].mid_numbers, result_7_till_13)
+        self.assertEqual(self.testes[11].mid_numbers, result_7_till_13)
+        self.assertEqual(self.testes[12].mid_numbers, result_7_till_13)
+        self.assertEqual(self.testes[13].mid_numbers, result_7_till_13)
 
     def test_final_sum(self):
-        first_input = {'signal': 1,
-                       'ten_potence': 1,
-                       'mid_numbers': [493825, 3950600, 29629500,
-                                       197530000, 987650000]}
-        second_input = {key: first_input[key] for key in first_input}
-        second_input['signal'] = -1
+        self.assertEqual(self.testes[0].mid_sum, '0')
+        self.assertEqual(self.testes[1].mid_sum, '16')
+        self.assertEqual(self.testes[2].mid_sum, '100')
+        self.assertEqual(self.testes[3].mid_sum, '12321')
+        self.assertEqual(self.testes[4].mid_sum, '12345678987654321')
+        self.assertEqual(self.testes[5].mid_sum, '12345678987654321')
+        self.assertEqual(self.testes[6].mid_sum, '12345678987654321')
+        self.assertEqual(self.testes[7].mid_sum, '1219253925')
+        self.assertEqual(self.testes[8].mid_sum, '1219253925')
+        self.assertEqual(self.testes[9].mid_sum, '1219253925')
+        self.assertEqual(self.testes[10].mid_sum, '1219253925')
+        self.assertEqual(self.testes[11].mid_sum, '1219253925')
+        self.assertEqual(self.testes[12].mid_sum, '1219253925')
+        self.assertEqual(self.testes[13].mid_sum, '1219253925')
 
-        third_input = {key: second_input[key] for key in second_input}
-        third_input['ten_potence'] = 100
+        self.assertEqual(self.testes[0].result, '+0')
+        self.assertEqual(self.testes[1].result, '+16')
+        self.assertEqual(self.testes[2].result, '+100')
+        self.assertEqual(self.testes[3].result, '+12321')
+        self.assertEqual(self.testes[4].result, '+12345678987654321')
+        self.assertEqual(self.testes[5].result, '-12345678987654321')
+        self.assertEqual(self.testes[6].result, '+12345678987.654321')
+        self.assertEqual(self.testes[7].result, '+1219253925')
+        self.assertEqual(self.testes[8].result, '-1219253925')
+        self.assertEqual(self.testes[9].result, '+1219253925')
+        self.assertEqual(self.testes[10].result, '+1219253.925')
+        self.assertEqual(self.testes[11].result, '-1219253.925')
+        self.assertEqual(self.testes[12].result, '+1219253.925')
+        self.assertEqual(self.testes[13].result, '-121.9253925')
 
-        fourth_input = {'signal': 1, 'ten_potence': 1, 'mid_numbers': [0]}
-        fifth_input = {'signal': -1, 'ten_potence': 10000, 'mid_numbers': [0]}
+    def test_len(self):
+        self.assertEqual(len(self.testes[0]), 2)
+        self.assertEqual(len(self.testes[1]), 3)
+        self.assertEqual(len(self.testes[2]), 4)
+        self.assertEqual(len(self.testes[3]), 6)
+        self.assertEqual(len(self.testes[4]), 18)
+        self.assertEqual(len(self.testes[5]), 18)
+        self.assertEqual(len(self.testes[6]), 19)
+        self.assertEqual(len(self.testes[7]), 11)
+        self.assertEqual(len(self.testes[8]), 11)
+        self.assertEqual(len(self.testes[9]), 11)
+        self.assertEqual(len(self.testes[10]), 12)
+        self.assertEqual(len(self.testes[11]), 12)
+        self.assertEqual(len(self.testes[12]), 12)
+        self.assertEqual(len(self.testes[13]), 12)
 
-        self.assertEqual(mult_escola_enzo.final_sum(first_input), 1219253925)
-        self.assertEqual(mult_escola_enzo.final_sum(second_input), -1219253925)
-        self.assertEqual(mult_escola_enzo.final_sum(third_input), -12192539.25)
-        self.assertEqual(mult_escola_enzo.final_sum(fourth_input), 0)
-        self.assertEqual(mult_escola_enzo.final_sum(fifth_input), 0)
+    def test_include_decimal(self):
+        check_list = [self.testes[6].include_decimal(number, spaces)
+                      for spaces, number in enumerate(self.testes[6].mid_numbers)]
 
-    def test_school_multiplication(self):
-        self.assertEqual(mult_escola_enzo.school_multiplication(98765, 12345), 1219253925)
-        self.assertEqual(mult_escola_enzo.school_multiplication(987.65, 1234.5), 1219253.925)
-        self.assertEqual(mult_escola_enzo.school_multiplication(-98765, 12345), -1219253925)
-        self.assertEqual(mult_escola_enzo.school_multiplication(987.65, -1.2345), -1219.253925)
-        self.assertEqual(mult_escola_enzo.school_multiplication(-98765, -12345), 1219253925)
+        result = ['1'*9]*3
+        result = [number + ' ' for number in result]
+        result = ['1'*(9-number)+'.'+'1'*(decimal-3)
+                  for number, decimal in zip(range(6, 0, -1), range(9, 3, -1))] + result
+        result = [number + ' '*spaces for spaces, number in enumerate(result)]
 
-        self.assertEqual(mult_escola_enzo.school_multiplication('98765', '12345'), 1219253925)
-        self.assertEqual(mult_escola_enzo.school_multiplication('987.65', '1234.5'), 1219253.925)
-        self.assertEqual(mult_escola_enzo.school_multiplication('-98765', '12345'), -1219253925)
-        self.assertEqual(mult_escola_enzo.school_multiplication('987.65', '-1.2345'), -1219.253925)
-        self.assertEqual(mult_escola_enzo.school_multiplication('98765', '-12345'), -1219253925)
-        self.assertEqual(mult_escola_enzo.school_multiplication('-98765', '-12345'), 1219253925)
+        self.assertEqual(check_list, result)
 
-        self.assertEqual(mult_escola_enzo.school_multiplication(0, 0), 0)
+    def test_str(self):
+        result_0 = '\n'.join(['   0',
+                              'x  0',
+                              '-----',
+                              '  +0',
+                              '-----',
+                              '  +0'])
 
-        for test in range(10):
-            number_1 = randint(-10**9, 10**9)
-            number_2 = randint(-10**9, 10**9)
+        result_1 = '\n'.join(['    4',
+                              'x   4',
+                              '------',
+                              '  +16',
+                              '------',
+                              '  +16'])
 
-            multiplication = float(number_1 * number_2)
-            self.assertEqual(mult_escola_enzo.school_multiplication(
-                number_1, number_2), multiplication)
-            self.assertEqual(mult_escola_enzo.school_multiplication(
-                str(number_1), str(number_2)), multiplication)
+        result_2 = '\n'.join(['    10',
+                              'x   10',
+                              '-------',
+                              '    +0',
+                              '  +10 ',
+                              '-------',
+                              '  +100'])
+
+        result_3 = '\n'.join(['     111',
+                              'x    111',
+                              '---------',
+                              '    +111',
+                              '   +111 ',
+                              '  +111  ',
+                              '---------',
+                              '  +12321'])
+
+        result_7 = '\n'.join(['        98765',
+                              'x       12345',
+                              '--------------',
+                              '      +493825',
+                              '     +395060 ',
+                              '    +296295  ',
+                              '   +197530   ',
+                              '   +98765    ',
+                              '--------------',
+                              '  +1219253925'])
+
+        result_8 = '\n'.join(['        98765',
+                              'x      -12345',
+                              '--------------',
+                              '      -493825',
+                              '     -395060 ',
+                              '    -296295  ',
+                              '   -197530   ',
+                              '   -98765    ',
+                              '--------------',
+                              '  -1219253925'])
+
+        result_13 = '\n'.join(['        98.765',
+                               'x      -1.2345',
+                               '---------------',
+                               '    -0.0493825',
+                               '    -0.395060 ',
+                               '    -2.96295  ',
+                               '   -19.7530   ',
+                               '   -98.765    ',
+                               '---------------',
+                               '  -121.9253925'])
+
+        self.assertEqual(str(self.testes[0]), result_0)
+        self.assertEqual(str(self.testes[1]), result_1)
+        self.assertEqual(str(self.testes[2]), result_2)
+        self.assertEqual(str(self.testes[3]), result_3)
+        self.assertEqual(str(self.testes[7]), result_7)
+        self.assertEqual(str(self.testes[8]), result_8)
+        self.assertEqual(str(self.testes[13]), result_13)
 
 
 if __name__ == '__main__':
